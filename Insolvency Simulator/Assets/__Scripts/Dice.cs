99c485d669f74663822503f81cfd90bbf3558ae5
DylanLoftus <dylanloftus@hotmail.com>
2019-09-29 23:34:45 +0100

    Made Dice script

---
+++ Insolvency Simulator/Assets/__Scripts/Dice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private int Roll()
+    {
+        int MIN = 1;
+        int MAX = 6;
+        int d1, d2;
+        int rollNumber;
+
+        System.Random rndm = new System.Random();
+
+        d1 = rndm.Next(MIN, MAX);
+        d2 = rndm.Next(MIN, MAX);
+
+        CheckDoubleRoll(d1, d2);
+
+        rollNumber = d1 + d2;
+
+        return rollNumber;
+    }
+
+    private bool CheckDoubleRoll(int d1, int d2)
+    {
+        bool doubleJump;
+
+        if (d1.Equals(d2))
+        {
+            doubleJump = true;
+        }
+        else
+        {
+            doubleJump = false;
+        }
+
+        return doubleJump;
+    }
+}
