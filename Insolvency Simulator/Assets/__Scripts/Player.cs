82a2cca44c73d099dc52288c86c4d5fb40fc43e7
DylanLoftus <dylanloftus@hotmail.com>
2019-09-30 14:58:58 +0100

    added property and player scripts

---
+++ Insolvency Simulator/Assets/__Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private string name;
+    private int money = 1500;
+    private Vector2 position;
+    private GameObject token;
+    List<Property> propertiesOwned = new List<Property>();
+
+    public Player(string name, int money, Vector2 position, GameObject token,List<Property> propertiesOwned)
+    {
+        this.name = name;
+        this.money = money;
+        this.position = position;
+        this.token = token;
+        this.propertiesOwned = propertiesOwned;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Insolvency Simulator/Assets/__Scripts/Property.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Property : MonoBehaviour
+{
+    private string propertyName;
+    private int propertyFaceValue;
+    private int houseCost;
+    private string propertyColour;
+
+    public Property(string propertyName, int propertyFaceValue, int houseCost, string propertyColour)
+    {
+        this.propertyName = propertyName;
+        this.propertyFaceValue = propertyFaceValue;
+        this.houseCost = houseCost;
+        this.propertyColour = propertyColour;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
