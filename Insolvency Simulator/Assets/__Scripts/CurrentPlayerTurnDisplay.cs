05873a9b61b8b20505191459f0f5768747f4fe4b
DylanLoftus <dylanloftus@hotmail.com>
2019-10-24 16:52:34 +0100

    Added current player display to show which player's turn it is

---
+++ Insolvency Simulator/Assets/__Scripts/CurrentPlayerTurnDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentPlayerTurnDisplay : MonoBehaviour
+{
+    // Instance Variables
+    GameManager theGameManager;
+    Text currentPlayerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get a reference for the game manager and the text element.
+        theGameManager = GameObject.FindObjectOfType<GameManager>();
+        currentPlayerText = GetComponent<Text>();
+    }
+
+    // Prints out the words instead of the number.
+    string[] numberWords = {"One", "Two", "Three" };
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Updates the text component depending on which player's turn it is.
+        currentPlayerText.text = "Current Player: " + numberWords[theGameManager.CurrentPlayerID];
+    }
+}
